7b6cb51fdef74a4a98c5000f5e132243a30aba09
jt4000 <johan.truong12@gmail.com>
2019-11-03 21:54:53 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ef;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly BloggingContext _context;
+
+        /*
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        */
+
+        public HomeController(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var ddf = _context.Blogs;
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ef/BloggingContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ef.Model;
+
+namespace ef
+{
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext() : base("name=BloggingContext")
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
